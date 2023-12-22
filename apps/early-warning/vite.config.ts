933fe517297bfc84a6954dc713018bf884fb9d1d
valerio-pescatori-acn <valerio.pescatori@accenture.com>
2023-12-22 17:41:26 +0100

    chore: setup vite proxy, remove proxy server
    moves fonts to tailwind config packages

--- apps/early-warning/vite.config.ts
+++ apps/early-warning/vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
 	plugins: [react()],
 	base: "/bandiregis/",
+	server: {
+		proxy: {
+			"/sap": {
+				changeOrigin: true,
+				target: "http://10.38.127.95:8000",
+			},
+		},
+	},
 });
---
+++ packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,70 @@
+import type { Config } from 'tailwindcss';
+import lineClamp from '@tailwindcss/line-clamp';
+
+const withOpacity = (variableName: string): string =>
+  `rgba(var(${variableName}), <alpha-value>)`;
+
+// We want each package to be responsible for its own content.
+const config: Omit<Config, 'content'> = {
+  plugins: [lineClamp],
+  theme: {
+    extend: {
+      colors: {
+        'base-black': withOpacity('--base-black'),
+        'base-white': withOpacity('--base-white'),
+        primary: withOpacity('--primary'),
+        'primary-1': withOpacity('--primary-1'),
+        'primary-2': withOpacity('--primary-2'),
+        'primary-3': withOpacity('--primary-3'),
+        'primary-5': withOpacity('--primary-5'),
+        'primary-6': withOpacity('--primary-6'),
+        'primary-7': withOpacity('--primary-7'),
+        'blue-light': withOpacity('--blue-light'),
+        'blue-lighter': withOpacity('--blue-lighter'),
+        'blue-dark': withOpacity('--blue-dark'),
+        'red-bar': withOpacity('--red-bar'),
+        'yellow-bar': withOpacity('--yellow-bar'),
+        'green-bar': withOpacity('--green-bar'),
+        'slate-44': withOpacity('--slate-44'),
+        error: withOpacity('--error'),
+        alert: withOpacity('--alert'),
+        warning: withOpacity('--warning'),
+        success: withOpacity('--success'),
+      },
+      gridTemplateColumns: {
+        measurments:
+          'minmax(0, 2fr) minmax(0, 1fr) minmax(0, 2fr) minmax(0, 1fr)',
+      },
+      boxShadow: {
+        tile: '0px 2px 8px 0px rgba(0, 0, 0, 0.10), 0px 0px 0px 1px rgba(0, 0, 0, 0.10);',
+      },
+      maxWidth: {
+        '7xl': '1496px',
+      },
+      spacing: {
+        30: '120px',
+        132: '528px',
+      },
+      fontSize: {
+        xs: ['12px', '28px'],
+        sm: ['14px', '24px'],
+        lg: ['24px', '36px'],
+        '4xl': ['40px', '54px'],
+      },
+      fontFamily: {
+        sans: ['"72"', 'sans-serif'],
+      },
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1150px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
+    plugins: [],
+  },
+};
+
+export default config;
