d819bcfb034ff96bd48d51e171f1624e42335fd2
valerio-pescatori-acn <valerio.pescatori@accenture.com>
2023-12-22 16:01:47 +0100

    chore: monorepo setup wip
    shared tsconfig working

--- apps/proxy-server/server.js
+++ apps/proxy-server/server.js
@@ -10,21 +10,21 @@ app.use(express.json());
 app.use(cors()); // Questo abilita CORS per tutte le risorse. Per configurazioni più specifiche, consulta la documentazione di cors.
 
 app.all("/*", async (req, res) => {
-	try {
-		const url = TARGET_URL + req.originalUrl;
-		const response = await axios({
-			method: req.method,
-			url: url,
-			data: req.body,
-			headers: { ...req.headers, host: null }, // Assicurati di rimuovere l'header 'host'
-		});
+  try {
+    const url = TARGET_URL + req.originalUrl;
+    const response = await axios({
+      method: req.method,
+      url: url,
+      data: req.body,
+      headers: { ...req.headers, host: null }, // Assicurati di rimuovere l'header 'host'
+    });
 
-		res.send(response.data);
-	} catch (error) {
-		res.status(500).send(error.message);
-	}
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(PORT, () => {
-	console.log(`Server in ascolto sulla porta ${PORT}`);
+  console.log(`Server in ascolto sulla porta ${PORT}`);
 });
